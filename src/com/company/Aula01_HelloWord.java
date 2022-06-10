5494f50ebfd4a8adc8ece2a99beafdfd248069eb
alexandre gomes <alecxandy@gmail.com>
2022-06-10 17:25:54 -0300

    Initial commit

---
+++ src/com/company/Aula01_HelloWord.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Aula01_HelloWord {
+
+    public static void main(String[] args) {
+        System.out.println("Hello word");
+    }
+}
---
+++ src/com/company/Aula02_TiposPrimitivos.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Aula02_TiposPrimitivos {
+    public static void main(String args[]) {
+
+        //int, double, char, byte, short, long, boolean
+        int idade = 10;
+        System.out.println(idade);
+    }
+}
