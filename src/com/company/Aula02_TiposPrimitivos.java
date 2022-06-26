0e6387c09386dba50f641753c035b8b2a0e8a030
alexandre gomes <alecxandy@gmail.com>
2022-06-26 14:00:08 -0300

    tipos primitivos

--- src/com/company/Aula02_TiposPrimitivos.java
+++ src/com/company/Aula02_TiposPrimitivos.java
@@ -4,7 +4,20 @@ public class Aula02_TiposPrimitivos {
     public static void main(String args[]) {
 
         //int, double, char, byte, short, long, boolean
-        int idade = 10;
-        System.out.println(idade);
+
+        int idadeInt  = 1000;
+        long idadeLong = 1000000000000000l; //usar a letra no final do numero
+        double salarioDouble = 2000D;
+        float salarioFloat = 2500.0F;
+        byte idadeByte = 10;
+        short idadeShort = 100;
+        char caracter = 'm';
+        boolean verdade = true;
+        String stg = "Alexandre gomes";
+
+
+        System.out.println(caracter);
+        System.out.println(idadeInt);
+        System.out.println();
     }
 }
---
+++ src/com/company/Aula03_TiposPrimitivosExercicio.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Aula03_TiposPrimitivosExercicio {
+
+    public static void main(String args[]){
+        String nome = "alexandre";
+        String  endereco = "rua professor";
+        double salario = 5.555D;
+    }
+}
